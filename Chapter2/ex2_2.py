38cf553e85e226d1166c0f711cd84e28945e52d5
Volodymyr Miroshnyk <vmiroshnik301290@gmail.com>
2017-06-29 19:46:07 +0300

    Ex 2.2 Counter plot and Surface plot for comparison

--- Chapter2/ex2_2.py
+++ Chapter2/ex2_2.py
@@ -1,2 +1,30 @@
-import matplotlib as mp
+import matplotlib.pyplot as plt
+import numpy as np
 
+"""Counter plot and Surface plot for comparison"""
+
+x1 = x2 = np.linspace(-100, 100, 20)
+
+X, Y = np.meshgrid(x1, x2)
+
+Z = 8 * X + 12 * Y + np.power(X, 2) - 2 * np.power(Y, 2)
+
+plt.figure()
+cp = plt.contour(X, Y, Z)
+plt.clabel(cp, inline=True,
+          fontsize=10)
+plt.title('Contour Plot')
+plt.xlabel('x')
+plt.ylabel('y')
+
+plt.show()
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.plot_surface(X, Y, Z)
+
+ax.set_xlabel('X Label')
+ax.set_ylabel('Y Label')
+ax.set_zlabel('Z Label')
+
+plt.show()
